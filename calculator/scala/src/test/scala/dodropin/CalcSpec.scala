84fefddf574642ae5ec85c05f9db3707c3832fd0
Charles Donaldson <charles.j.donaldson@gmail.com>
2024-11-24 22:06:47 -0500

    feat: Calculator: Scala: exponents implemented

--- calculator/scala/src/test/scala/dodropin/CalcSpec.scala
+++ calculator/scala/src/test/scala/dodropin/CalcSpec.scala
@@ -18,46 +18,58 @@ class CalcSpec extends munit.FunSuite {
   }
 
   test("calc 4 - 3 returns 1") {
-    assertEquals(dodropin.CalcOps("4 - 3"), "1")
+    assertEquals(CalcOps("4 - 3"), "1")
   }
 
   test("calc 4 * 3 returns 12") {
-    assertEquals(dodropin.CalcOps("4 * 3"), "12")
+    assertEquals(CalcOps("4 * 3"), "12")
   }
 
   test("calc 12 / 3 returns 4") {
-    assertEquals(dodropin.CalcOps("12 / 3"), "4")
+    assertEquals(CalcOps("12 / 3"), "4")
   }
 
   test("calc 4 * 3.0 returns 12.0") {
-    assertEquals(dodropin.CalcOps("4 * 3.0"), "12.0")
+    assertEquals(CalcOps("4 * 3.0"), "12.0")
   }
 
   test("calc 12 / 3.0 returns 4.0") {
-    assertEquals(dodropin.CalcOps("12 / 3.0"), "4.0")
+    assertEquals(CalcOps("12 / 3.0"), "4.0")
   }
 
   test("calc 3 + 2 + 5 returns 10") {
-    assertEquals(dodropin.CalcOps("3 + 2 + 5"), "10")
+    assertEquals(CalcOps("3 + 2 + 5"), "10")
   }
 
   test("calc 3 * 2 + 5 returns 11") {
-    assertEquals(dodropin.CalcOps("3 * 2 + 5"), "11")
+    assertEquals(CalcOps("3 * 2 + 5"), "11")
   }
 
   test("calc 1 - 3 * 2 + 5 returns -10") {
-    assertEquals(dodropin.CalcOps("1 - 3 * 2 + 5"), "-10")
+    assertEquals(CalcOps("1 - 3 * 2 + 5"), "-10")
   }
 
   test("calc 3*(2 + 5) returns 21") {
-    assertEquals(dodropin.CalcOps("3*(2 + 5)"), "21")
+    assertEquals(CalcOps("3*(2 + 5)"), "21")
   }
 
   test("calc 3*{ 2 + 5 } returns 21") {
-    assertEquals(dodropin.CalcOps("3*{ 2 + 5 }"), "21")
+    assertEquals(CalcOps("3*{ 2 + 5 }"), "21")
   }
 
   test("calc 3*[ 2 + 5 ] returns 21") {
-    assertEquals(dodropin.CalcOps("3*[ 2 + 5 ]"), "21")
+    assertEquals(CalcOps("3*[ 2 + 5 ]"), "21")
+  }
+
+  test("calc 2^5 returns 32") {
+    assertEquals(CalcOps("2^5"), "32")
+  }
+
+  test("calc 2^5.0 returns 32.0") {
+    assertEquals(CalcOps("2^5.0"), "32.0")
+  }
+
+  test("calc (3-1)^5 returns 32") {
+    assertEquals(CalcOps("(3-1)^5"), "32")
   }
 }
